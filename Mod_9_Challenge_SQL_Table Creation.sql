20bfe6e0ef2b7e843ce6e6298f4c2ca0ea7ee447
Caitlyn Myland <caitlyn.myland@gmail.com>
2023-11-27 22:20:00 -0500

    second commit

--- Mod_9_Challenge_SQL_Table Creation.sql
+++ Mod_9_Challenge_SQL_Table Creation.sql
@@ -1,58 +1,82 @@
 -- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/8e4QYF
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-DROP TABLE IF EXISTS Titles CASCADE;
-DROP TABLE IF EXISTS Departments CASCADE;
-DROP TABLE IF EXISTS Dept_emp CASCADE;
-DROP TABLE IF EXISTS Salaries CASCADE;
-DROP TABLE IF EXISTS Dept_manager CASCADE;
-DROP TABLE IF EXISTS Employees CASCADE;
 
-CREATE TABLE Titles (
-    title_id VARCHAR(10) NOT NULL PRIMARY KEY,
-    title VARCHAR(30) NOT NULL
-);
-CREATE TABLE Employees (
-    emp_no INT NOT NULL PRIMARY KEY,
-    emp_title_id VARCHAR(10) NOT NULL,
-    birth_date DATE NOT NULL,
-    first_name VARCHAR(15) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    sex CHAR NOT NULL,
-    hire_date DATE NOT NULL,
-    FOREIGN KEY (emp_title_id) REFERENCES Titles(title_id)
-);
-CREATE TABLE Departments (
-    dept_no VARCHAR(10) NOT NULL PRIMARY KEY,
-    dept_name VARCHAR(30) NOT NULL
+
+CREATE TABLE "Departments" (
+    "dept_no" varchar   NOT NULL,
+    "dept_name" varchar(30)   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
+     )
 );
 
-CREATE TABLE Dept_manager (
-    dept_no VARCHAR(10) NOT NULL,
-    emp_no INT NOT NULL,
-    PRIMARY KEY (dept_no,emp_no),
-	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
-	FOREIGN KEY (dept_no) REFERENCES Departments(dept_no)
+CREATE TABLE "Salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" money   NOT NULL,
+    CONSTRAINT "pk_Salaries" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
-CREATE TABLE Dept_emp (
-    emp_no INT NOT NULL,
-    dept_no VARCHAR(10) NOT NULL,
-	Primary KEY (emp_no,dept_no),
-	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
-	FOREIGN KEY (dept_no) REFERENCES Departments(dept_no)
+CREATE TABLE "Dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar   NOT NULL,
+    CONSTRAINT "pk_Dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
 );
 
-CREATE TABLE Salaries (
-    emp_no INT NOT NULL PRIMARY KEY,
-    salary NUMERIC NOT NULL, -- Assuming salary is a numeric value
-    FOREIGN KEY (emp_no) REFERENCES Employees(emp_no)
+CREATE TABLE "Dept_manager" (
+    "dept_no" varchar   NOT NULL,
+    "emp_no" int   NOT NULL,
+    CONSTRAINT "pk_Dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
 );
 
-COPY Departments FROM 'C:\Program Files\PostgreSQL\14\pgAdmin 4\runtime\data\departments.csv' WITH CSV HEADER;
-COPY Titles FROM 'C:\Program Files\PostgreSQL\14\pgAdmin 4\runtime\data\titles.csv' WITH CSV HEADER;
-COPY Employees FROM 'C:\Program Files\PostgreSQL\14\pgAdmin 4\runtime\data\employees.csv' WITH CSV HEADER;
-COPY Dept_manager FROM 'C:\Program Files\PostgreSQL\14\pgAdmin 4\runtime\data\dept_manager.csv' WITH CSV HEADER;
-COPY Dept_emp FROM 'C:\Program Files\PostgreSQL\14\pgAdmin 4\runtime\data\dept_emp.csv' WITH CSV HEADER;
-COPY Salaries FROM 'C:\Program Files\PostgreSQL\14\pgAdmin 4\runtime\data\salaries.csv' WITH CSV HEADER;
+CREATE TABLE "Titles" (
+    "title_id" varchar   NOT NULL,
+    "title" varchar(30)   NOT NULL,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "title_id"
+     )
+);
 
+CREATE TABLE "Employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" varchar   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar(15)   NOT NULL,
+    "last_name" varchar(20)   NOT NULL,
+    "sex" char   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "Titles" ("title_id");
+
+COPY "Departments" FROM 'C:\Program Files\PostgreSQL\14\pgAdmin 4\runtime\data\departments.csv' WITH CSV HEADER;
+COPY "Titles" FROM 'C:\Program Files\PostgreSQL\14\pgAdmin 4\runtime\data\titles.csv' WITH CSV HEADER;
+COPY "Employees" FROM 'C:\Program Files\PostgreSQL\14\pgAdmin 4\runtime\data\employees.csv' WITH CSV HEADER;
+COPY "Dept_manager" FROM 'C:\Program Files\PostgreSQL\14\pgAdmin 4\runtime\data\dept_manager.csv' WITH CSV HEADER;
+COPY "Dept_emp" FROM 'C:\Program Files\PostgreSQL\14\pgAdmin 4\runtime\data\dept_emp.csv' WITH CSV HEADER;
+COPY "Salaries" FROM 'C:\Program Files\PostgreSQL\14\pgAdmin 4\runtime\data\salaries.csv' WITH CSV HEADER;
